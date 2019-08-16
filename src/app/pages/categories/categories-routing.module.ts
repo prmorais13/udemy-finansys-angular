55155e59ba0186b3abb1f85ccd3dfb115f42a1c3
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-08-16 19:27:08 -0300

    Aula 9. Criando o módulo de Categorias

---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoryListComponent
+  },
+
+  {
+    path: ':id/edit',
+    component: CategoryFormComponent
+  },
+  {
+    path: 'new',
+    component: CategoryFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule {}
