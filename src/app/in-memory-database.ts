73ba1e8a8f9f12cc0ae54b5396a79d4b32b0e30c
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-08-16 20:58:41 -0300

    Aula 13. Adicionando o model 'Category'

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,5 +1,6 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Observable, of } from 'rxjs';
+import { CategoryModel } from './pages/categories/model/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   // createDb(): Observable<any> {
@@ -13,7 +14,7 @@ export class InMemoryDatabase implements InMemoryDbService {
   //   return of(categories);
   // }
   createDb() {
-    const categories = [
+    const categories: CategoryModel[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos contas da casa' },
       { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios' },
       { id: 3, name: 'Lazer', description: 'Cinema, parque, praia, bar, etc' },
@@ -21,6 +22,6 @@ export class InMemoryDatabase implements InMemoryDbService {
       { id: 1, name: 'Extra', description: 'Trabalhos extras' }
     ];
 
-    return { categories };
+    return categories;
   }
 }
