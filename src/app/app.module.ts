9d8ef9498086cf58248f5b3ca7a9c518fb35b46e
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-08-16 23:26:14 -0300

    Aula 15. Ativando o interceptador HTTP do AngularInMemoryWebAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
