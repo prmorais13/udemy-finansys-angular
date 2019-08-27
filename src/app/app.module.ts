c8230dbf99ad9315134cc6fffdf8289a6cf2e998
Paulo Roberto <prmorais1302@gmail.com>
2019-08-27 16:10:32 -0300

    Aulas 36 a 38 - Reestruturando os Módulos - Com erro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,22 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core'
 // import { PCalendarPtbrModule } from 'p-calendar-ptbr';
-
 // import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 // import { InMemoryDatabase } from './in-memory-database';
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
+    CoreModule,
+    AppRoutingModule
     // PCalendarPtbrModule,
-    BrowserAnimationsModule
-
     // HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-br' }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule]
+})
+export class CoreModule {}
