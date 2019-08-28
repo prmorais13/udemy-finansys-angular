f4a1e1753ba76b90161b7dca819079aa5aecc9dd
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-08-28 00:43:12 -0300

    Aulas 40. Criando e integrando a classe 'BaseResourceService'

--- src/app/pages/categories/service/category.service.ts
+++ src/app/pages/categories/service/category.service.ts
@@ -1,52 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
 
 import { CategoryModel } from '../model/category.model';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
-  private readonly BASE_URL = 'api/categories';
-  private readonly BASE_URI = 'http://localhost:3000/categories';
+export class CategoryService extends BaseResourceService<CategoryModel> {
+  // private readonly BASE_URL = 'api/categories';
+  // private readonly BASE_URI = 'http://localhost:3000/categories';
 
-  constructor(private http: HttpClient) {}
-
-  getCategories(): Observable<CategoryModel[]> {
-    return this.http.get<CategoryModel[]>(this.BASE_URI);
-  }
-
-  getCategory(id: number | string): Observable<CategoryModel> {
-    return this.http
-      .get<CategoryModel>(`${this.BASE_URI}/${id}`)
-      .pipe(catchError(this.handleError));
-  }
-
-  createCategory(category: CategoryModel): Observable<CategoryModel> {
-    return this.http
-      .post<CategoryModel>(this.BASE_URI, category)
-      .pipe(catchError(this.handleError));
-  }
-
-  updateCategory(category: CategoryModel): Observable<CategoryModel> {
-    return this.http
-      .put<CategoryModel>(`${this.BASE_URI}/${category.id}`, category)
-      .pipe(
-        catchError(this.handleError),
-        map(() => category)
-      );
-  }
-
-  deleteCategory(id: number | string) {
-    return this.http
-      .delete(`${this.BASE_URI}/${id}`)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: any): Observable<any> {
-    console.log('Erro na requisição!', error);
-    return throwError(error);
+  constructor(protected injector: Injector) {
+    super('http://localhost:3000/categories', injector);
   }
 }
--- src/app/shared/services/base-resource.service.ts
+++ src/app/shared/services/base-resource.service.ts
@@ -1,9 +1,58 @@
-import { Injectable } from '@angular/core';
+import { Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { BaseResourceModel } from '../models/base-resource.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BaseResourceService {
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+  protected http: HttpClient;
 
-  constructor() { }
+  constructor(protected BASE_URI: string, protected injector: Injector) {
+    this.http = injector.get(HttpClient);
+  }
+
+  getCategories(): Observable<T[]> {
+    return this.http.get<T[]>(this.BASE_URI).pipe(catchError(this.handleError));
+  }
+
+  getCategory(id: number | string): Observable<T> {
+    return this.http
+      .get<T>(`${this.BASE_URI}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  createCategory(resource: T): Observable<T> {
+    return this.http
+      .post<T>(this.BASE_URI, resource)
+      .pipe(catchError(this.handleError));
+  }
+
+  updateCategory(resource: T): Observable<T> {
+    return this.http.put<T>(`${this.BASE_URI}/${resource.id}`, resource).pipe(
+      catchError(this.handleError),
+      map(() => resource)
+    );
+  }
+
+  deleteCategory(id: number | string) {
+    return this.http
+      .delete(`${this.BASE_URI}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  protected handleError(error: any): Observable<any> {
+    console.log('Erro na requisição!', error);
+    return throwError(error);
+  }
+
+  // protected jsonDataToResources(jsonData: any[]): T[] {
+  //   const resources: T[] = [];
+
+  //   jsonData.forEach(el => {
+  //     const resource = Object.assign(new T(), el);
+  //     resources.push(resource);
+  //   });
+
+  //   return resources;
+  // }
 }
